795c07d41c55f9d7246ae309874a4a6f19480284
Gubanov Igor <applecake@inbox.ru>
2019-08-16 10:54:42 +0300

    сделал модели

---
+++ models/seeds.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const { Card } = require('./Card');
+
+mongoose.connect('mongodb://localhost:27017/jeopardy', { useNewUrlParser: true, useCreateIndex: true });
+
+async function mainFunction() {
+  const card1 = new Card({
+    title: 'Про Никиту',
+    questions: [
+      {
+        q: 'Кто говнюк?',
+        a: 'Никита',
+        p: 200,
+      },
+      {
+        q: 'Кто вонючка?',
+        a: 'Никита',
+        p: 400,
+      },
+      {
+        q: 'Кто всех достал?',
+        a: 'Никита',
+        p: 600,
+      },
+      {
+        q: 'Самое обидное слово в мире',
+        a: 'Никита',
+        p: 800,
+      },
+      {
+        q: 'Кто король?',
+        a: ' Не Никита',
+        p: 1000,
+      },
+    ]
+  });
+  await card1.save();
+}
+
+mainFunction();
